d90079a7080e12b8f08dee11883cbf09088f51c8
PiGuy2 <rsamm3@gmail.com>
2016-09-05 16:26:02 -0400

    Multiplayer Fix

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 	void Update ()
 	{
+		if (!isLocalPlayer) {
+			return;
+		}
+
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 100.0f;
 		var y = Input.GetAxis ("Vertical") * Time.deltaTime * -5.0f;
 
