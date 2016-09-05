0480efbce41f955bea14993c0b998b91721c0029
PiGuy2 <rsamm3@gmail.com>
2016-09-05 15:09:54 -0400

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	void Update ()
+	{
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 100.0f;
+		var y = Input.GetAxis ("Vertical") * Time.deltaTime * -5.0f;
+
+		transform.Rotate (0, x, 0);
+		transform.Translate (y, 0, 0);
+	}
+}
