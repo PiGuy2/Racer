e05bfefe1075b290bd1dc858e176cb9094656766
PiGuy2 <rsamm3@gmail.com>
2017-01-09 20:08:32 -0500

    Camera follow
    Camera rotates around player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
 	Vector3 diff;
 	Vector3 l;
 	Quaternion r;
+	public float ro;
+	public float curR;
+	public float lastR;
+	Vector3 n;
 
 	// Use this for initialization
 	void Start ()
@@ -22,13 +26,30 @@ public class CameraController : MonoBehaviour
 			return;
 		}
 
-		this.gameObject.transform.position = player.transform.position + diff;
+//		this.gameObject.transform.position = player.transform.position + diff;
+
+//		curR = player.transform.rotation.y;
+		//this.gameObject.transform.RotateAround (player.transform.position, Vector3.up, (curR - lastR) * 180);
+		//this.gameObject.transform.RotateAround (Vector3.up, player.transform.position, (curR - lastR) * 180);
+//		lastR = curR;
+	}
+
+	void LateUpdate ()
+	{
+		float desiredAngle = player.transform.eulerAngles.y + 90;
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+		n = player.transform.position - (rotation * diff);
+		n.y += 7;
+		transform.position = n;
+		transform.LookAt (player.transform);
 	}
 
 	public void Follow (GameObject followObject)
 	{
 		player = followObject;
 		diff = this.gameObject.transform.position - player.transform.position;
+		ro = followObject.transform.rotation.y;
+		lastR = ro;
 	}
 
 	public void Reset ()
